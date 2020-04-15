e335a738202d5f09d7dcbb36b2a868f2947fe53b
Ubuntu <ubuntu@ip-172-31-45-141.ap-northeast-2.compute.internal>
2020-04-15 09:06:57 +0000

    change relation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
     # dependent 필수
     has_many :likes, dependent: :destroy
-    has_many :users, through: :likes
+    has_many :liked_users, through: :likes, source: :user
     
     # dependent 필수
     has_many :comments, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
          
   # dependent 필수
   has_many :likes, dependent: :destroy
-  has_many :posts, through: :likes
+  has_many :liked_posts, through: :likes, source: :post
 end
 
